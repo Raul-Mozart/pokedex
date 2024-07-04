34421837a62a1f3f726f0eb43aab92dc93daad0e
Raul Mozart <raulmab@gmail.com>
2024-07-03 22:58:06 -0300

    Stats 50% e Filtro/Busca 100%

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -1,28 +1,23 @@
 const maxRecords = 151
 const limit = 10
 let offset = 0;
-let pokemonId = 1;
-const teste1 = document.querySelectorAll('.teste')
 const pokemonList = document.getElementById('pokemonList')
-const statsContent = document.getElementById('statsContent')
-let teste = 0;
-
+const loadMoreButton = document.getElementById('loadMoreButton')
+const form = document.getElementById('form')
 
 function convertPokemonToLi(pokemon) {
     return `
-        <li class="pokemon ${pokemon.type}">
-            <a href="stats.html" class="teste" data-id="${pokemon.number}">
-                <span class="number">#${pokemon.number}</span>
-                <span class="name">${pokemon.name}</span>
+        <li class="pokemon ${pokemon.type}" data-id="${pokemon.number}">
+            <span class="number">#${pokemon.number}</span>
+            <span class="name">${pokemon.name}</span>
 
-                <div class="detail">
-                    <ol class="types">
-                        ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
-                    </ol>
+            <div class="detail">
+                <ol class="types">
+                    ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+                </ol>
 
-                    <img src="${pokemon.img}" alt="${pokemon.name}">
-                </div>
-            </a>
+                <img src="${pokemon.img}" alt="${pokemon.name}">
+            </div>
         </li>
     `
 }
@@ -34,16 +29,51 @@ function loadPokemonItens(offset, limit) {
     })
 }
 
-teste1.forEach((pokemon) =>{
-    pokemon.addEventListener('click', () => {
-        console.log('dataId:', dataId)
-        teste = parseInt(dataId)
-        console.log('dataId:', teste)
-        localStorage.setItem('teste', teste);
-    })
-})
-
 loadPokemonItens(offset, limit)
+
+function Timeout(){
+    setTimeout(() => {
+    
+        let pokemon = document.querySelectorAll('.pokemon')
+          
+        // Adiciona um event listener de clique a cada card
+        pokemon.forEach(function(card) {
+            card.addEventListener('click', (event) =>{
+                var dataId = event.currentTarget.getAttribute('data-id');
+                loadPokemonStat(dataId,limitDetail)
+            });
+        });
+    
+        /*
+        +===========================+
+        +                           +
+        +       FILTRO/BUSCA        +
+        +                           +
+        +===========================+
+        */
+        let inputElement = document.querySelector("#ibusca");
+        let listElement = document.querySelector("#pokemonList");
+        let pokemonCards = listElement.querySelectorAll(".pokemon");
+
+        inputElement.addEventListener("input", (e) => {
+            let searchText = e.target.value.trim().toLowerCase();
+
+            pokemonCards.forEach(card => {
+                let nameElement = card.querySelector(".name");
+                let nameText = nameElement.textContent.toLowerCase();
+                let typeElements = card.querySelectorAll(".type");
+                let typesText = Array.from(typeElements).map(type => type.textContent.toLowerCase()).join(' ');
+
+                if (nameText.includes(searchText) || typesText.includes(searchText)) {
+                    card.style.display = "block";
+                } else {
+                    card.style.display = "none";
+                }
+            });
+        });
+
+    },500)   
+}
 
 loadMoreButton.addEventListener('click', () => {
     offset += limit
@@ -54,7 +84,54 @@ loadMoreButton.addEventListener('click', () => {
         loadPokemonItens(offset, newLimit)
 
         loadMoreButton.parentElement.removeChild(loadMoreButton)
+        Timeout()
     } else {
         loadPokemonItens(offset, limit)
+        Timeout()
     }
 })
+
+/*
++===========================+
++                           +
++           STATS           +
++                           +
++===========================+
+*/
+const limitDetail = 1
+const statsContent = document.getElementById('statsContent')
+
+function convertStatsPokemonToLi(pokemon){
+    return`
+        <div>
+            <a href="./index.html">
+                <-
+            </a>
+        </div>
+        <li>
+            <div>
+                <div>
+                    <img src="${pokemon.img}" alt="${pokemon.name}"
+                </div>
+                <div>
+                    <ul>
+                        ${pokemon.stats.map((stat) => `<li class="${stat}">${stat} <span>${pokemon.statNum}</span></li>`).join('')}
+                    </ul>
+                </div>
+            </div>
+        </li>
+    `
+}
+
+function loadPokemonStat(offset,limit){
+    pokeApi.getPokemons(offset - 1,limit).then((pokemons = []) => {
+        const newHtml = pokemons.map(convertStatsPokemonToLi).join('')
+        pokemonList.innerHTML = newHtml
+        form.style.display = 'none';
+        loadMoreButton.style.display = 'none';
+    })
+}
+
+
+
+Timeout()
